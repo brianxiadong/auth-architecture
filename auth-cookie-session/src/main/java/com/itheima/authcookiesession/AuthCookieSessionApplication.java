915e58e9b48a5a6924572589c645fcd4f1852267
itheimaxia <itheimaxia@itcast.cn>
2020-12-21 18:32:20 +0800

    提交基础结构

---
+++ auth-cookie-session/src/main/java/com/itheima/authcookiesession/AuthCookieSessionApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.authcookiesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCookieSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCookieSessionApplication.class, args);
+    }
+
+}
---
+++ auth-cookie-session/src/test/java/com/itheima/authcookiesession/AuthCookieSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.authcookiesession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthCookieSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
